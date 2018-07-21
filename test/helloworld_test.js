13b901a2e76a61472da6fc45cd0e49aaef64a202
Joshua Kim <kyh79@inka.co.kr>
2018-07-21 20:41:34 +0900

    aa

--- test/helloworld_test.js
+++ test/helloworld_test.js
@@ -2,7 +2,6 @@ var Browser = require('zombie')
 var assert = require('assert')
 var app = require('../helloworld')
 
-
 describe('main page', function() {
   before(function() {
     this.browser = new Browser({ site: 'http://localhost:3000' })
